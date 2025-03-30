9a729ac22b34986b84af9fadd04f511ef667a297
vasar80 <133333771+vasar80@users.noreply.github.com>
2025-03-30 01:40:07 +0100

    Update NeglectTest.jsx

--- src/components/NeglectTest.jsx
+++ src/components/NeglectTest.jsx
@@ -153,7 +153,7 @@ const NeglectTest = () => {
   return (
     <div className="test-container">
       <img 
-        src="/str-logo.png" 
+        src={process.env.PUBLIC_URL + '/str-logo.png'} 
         alt="Logo" 
         className="logo"
         style={{ maxWidth: '300px' }}
